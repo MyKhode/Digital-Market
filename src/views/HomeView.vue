74fefc4eb18e78cc69838c9f4a5d9ed3bb16cabe
Ikhode <132768132+MyKhode@users.noreply.github.com>
2025-01-04 14:15:25 +0700

    add project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { supabase } from "@/services/supabase";
+import PromoteSection from "@/components/PromoteSection.vue";
+import ProductCard from "@/components/ProductCard.vue";
+</script>
+
+<template>
+  <div>
+    <PromoteSection />
+    <ProductCard />
+    <!-- <ProductCard /> -->
+  </div>
+
+  <!-- <h1 class="text-3xl font-medium">Home</h1> -->
+  <!-- <pre>{{ supabase.auth.user() }}</pre> -->
+</template>
